157e9ac87cb8dfa275652c91a12fb20891204571
James Williams <james.l.williams@gmail.com>
2012-08-23 07:12:02 -0700

    Updated SpritePacker to use updated Chrome App API.

--- SpritePacker/js/background.js
+++ SpritePacker/js/background.js
@@ -1,9 +1,8 @@
-chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.appWindow.create('index.html', {
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
     'width': 1024,
     'height': 800
   }, function(id) {
-    console.log("here");                     
     window.application = new Application();   
   });
   
